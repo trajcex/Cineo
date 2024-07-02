b965a4f4cc438d75e36ea7f26f367f331b631fd9
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2024-07-02 16:28:31 +0200

    feat: Updated userPool

--- Backend/infrastructure/lib/cognito-statck.ts
+++ Backend/infrastructure/lib/cognito-statck.ts
@@ -6,7 +6,36 @@ export class CognitoStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const pool = new cognito.UserPool(this, "Pool");
+        const pool = new cognito.UserPool(this, "Pool", {
+            standardAttributes: {
+                givenName: {
+                    required: true,
+                },
+                familyName: {
+                    required: true,
+                },
+                birthdate: {
+                    required: true,
+                },
+                nickname: {
+                    required: true,
+                },
+                email: {
+                    required: true,
+                },
+            },
+            passwordPolicy: {
+                minLength: 12,
+                requireLowercase: true,
+                requireUppercase: true,
+                requireDigits: true,
+                requireSymbols: true,
+            },
+            selfSignUpEnabled: true,
+            signInAliases: { username: true },
+            autoVerify: { email: true },
+            signInCaseSensitive: false,
+        });
 
         const client = pool.addClient("app-client", {
             supportedIdentityProviders: [
