584b31c29ad06d5d644c055baa6d99f8dcd067aa
pejinovics <pejinovicstefan@gmail.com>
2024-07-04 23:35:55 +0200

    feat: add routing for login, home, add-movie

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AllMoviesComponent} from "./movies-page/all-movies/all-movies.component";
+import {AddMovieComponent} from "./movie/add-movie/add-movie.component";
+import {ViewMovieComponent} from "./movie/view-movie/view-movie.component";
+import {LogInComponent} from "./infrastructure/auth/log-in/log-in.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {path: "login", component: LogInComponent},
+  {path : "home", component : AllMoviesComponent},
+  {path : "add-movie", component : AddMovieComponent},
+  {path : "view-movie", component : ViewMovieComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
