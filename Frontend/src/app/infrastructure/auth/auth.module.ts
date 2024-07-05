ef656351462a2f9c5ac766f9c55ca075363aa8b7
pejinovics <pejinovicstefan@gmail.com>
2024-07-05 19:24:40 +0200

    feat: add routing to login

--- Frontend/src/app/infrastructure/auth/auth.module.ts
+++ Frontend/src/app/infrastructure/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { LogInComponent } from './log-in/log-in.component';
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 import {LayoutModule} from "../../layout/layout.module";
+import {RouterOutlet} from "@angular/router";
 
 @NgModule({
   declarations: [LogInComponent],
-  imports: [CommonModule, AmplifyAuthenticatorModule, LayoutModule],
+    imports: [CommonModule, AmplifyAuthenticatorModule, LayoutModule, RouterOutlet],
   exports: [LogInComponent],
 })
 export class AuthModule {}
