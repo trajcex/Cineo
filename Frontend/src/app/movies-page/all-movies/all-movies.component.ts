89c14c2f4890346f39f97585419c3bf9ba61e436
pejinovics <pejinovicstefan@gmail.com>
2024-07-05 22:04:16 +0200

    feat: add all movies static page

--- Frontend/src/app/movies-page/all-movies/all-movies.component.ts
+++ Frontend/src/app/movies-page/all-movies/all-movies.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Movie} from "../models/movie.model";
+import {AuthServiceService} from "../../service/auth-service.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-all-movies',
@@ -7,4 +10,64 @@ import { Component } from '@angular/core';
 })
 export class AllMoviesComponent {
 
+  role: string = '';
+  constructor(private auth: AuthServiceService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.role = this.auth.getCurrentRole();
+  }
+
+  movies: Movie[] = [
+    {
+      title: 'Movie 1',
+      description: 'Description for movie 1',
+      imageUrl: 'assets/images/netflix.png'
+    },
+    {
+      title: 'Movie 2',
+      description: 'Description for movie 2',
+      imageUrl: 'assets/images/netflix.png'
+    },
+    {
+      title: 'Movie 3',
+      description: 'Description for movie 3',
+      imageUrl: 'assets/images/netflix.png'
+    },
+    {
+      title: 'Movie 4',
+      description: 'Description for movie 4',
+      imageUrl: 'assets/images/netflix.png'
+    },
+    {
+      title: 'Movie 5',
+      description: 'Description for movie 5',
+      imageUrl: 'assets/images/netflix.png'
+    },
+    {
+      title: 'Movie 1',
+      description: 'Description for movie 1',
+      imageUrl: 'assets/images/netflix.png'
+    },
+    {
+      title: 'Movie 2',
+      description: 'Description for movie 2',
+      imageUrl: 'assets/images/netflix.png'
+    },
+    {
+      title: 'Movie 3',
+      description: 'Description for movie 3',
+      imageUrl: 'assets/images/netflix.png'
+    },
+    {
+      title: 'Movie 4',
+      description: 'Description for movie 4',
+      imageUrl: 'assets/images/netflix.png'
+    },
+    {
+      title: 'Movie 5',
+      description: 'Description for movie 5',
+      imageUrl: 'assets/images/netflix.png'
+    }
+  ];
 }
