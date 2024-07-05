08f406328569a3ab57afed0246ebb80db143c0a4
pejinovics <pejinovicstefan@gmail.com>
2024-07-05 19:25:44 +0200

    feat: add page for add movie

--- Frontend/src/app/movies-page/movies-page.module.ts
+++ Frontend/src/app/movies-page/movies-page.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AllMoviesComponent } from './all-movies/all-movies.component';
-import { AdminMoviesComponent } from './admin-movies/admin-movies.component';
+import {LayoutModule} from "../layout/layout.module";
 
 
 
 @NgModule({
   declarations: [
     AllMoviesComponent,
-    AdminMoviesComponent
   ],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        LayoutModule
+    ]
 })
 export class MoviesPageModule { }
