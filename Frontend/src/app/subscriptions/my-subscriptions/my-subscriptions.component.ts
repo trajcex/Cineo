821ff2c917402628d9807cbd1803b0e9d4f35e1f
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2024-07-06 18:35:58 +0200

    feat: Display subscription

--- Frontend/src/app/subscriptions/my-subscriptions/my-subscriptions.component.ts
+++ Frontend/src/app/subscriptions/my-subscriptions/my-subscriptions.component.ts
@@ -1,4 +1,6 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AuthServiceService } from 'src/app/service/auth-service.service';
 import { LambdaService } from 'src/app/service/lambda.service';
 
 @Component({
@@ -7,5 +9,40 @@ import { LambdaService } from 'src/app/service/lambda.service';
   styleUrls: ['./my-subscriptions.component.css'],
 })
 export class MySubscriptionsComponent {
-  constructor(private lambdaService: LambdaService) {}
+  actors: string[] = [];
+  genres: string[] = [];
+  directors: string[] = [];
+  subscriptionsActors: string[] = [];
+  subscriptionsGenres: string[] = [];
+  subscriptionsDirectors: string[] = [];
+
+  constructor(
+    private lambdaService: LambdaService,
+    private auth: AuthServiceService
+  ) {}
+
+  ngOnInit() {
+    this.lambdaService.getPossibleSubscriptions().subscribe({
+      next: (result) => {
+        this.actors = result.actors;
+        this.genres = result.genres;
+        this.directors = result.directors;
+        console.log('GetPossibleSubscriptions result:', result);
+      },
+      error: (error: HttpErrorResponse) => {
+        console.error('Error:', error);
+      },
+    });
+    this.lambdaService.getSubscriptions().subscribe({
+      next: (result) => {
+        this.subscriptionsActors = result.actors;
+        this.subscriptionsGenres = result.genres;
+        this.subscriptionsDirectors = result.directors;
+        console.log('Get :', result);
+      },
+      error: (error: HttpErrorResponse) => {
+        console.error('GetSubscriptions error:', error);
+      },
+    });
+  }
 }
